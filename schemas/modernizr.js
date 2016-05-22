c7be1756178098d91f476b0e6e49994cac2994b2
Aram Baghdasaryan <xquack@gmail.com>
2016-05-22 14:08:01 +0400

    Schema for modernizr is ready

---
+++ schemas/modernizr.js
@@ -0,0 +1,30 @@
+exports.sch = {
+	uuid: 'modernizr',
+	columns: {
+		name: 'Modernizr',
+		author: 'Stu Cox, Paul Irish',
+		url: 'http://modernizr.com/',
+		version: '',
+		pinnedVersion: '',
+		isSubscribed: 0
+	},
+	versionUrl: 'https://raw.githubusercontent.com/Modernizr/Modernizr/master/package.json',
+	parseVersion: function(body) {
+		var re = /"version": "(\d+\.\d+\.\d+)"/gi;
+		var m;
+
+		while ((m = re.exec(body)) !== null) {
+			if (m.index === re.lastIndex) {
+				re.lastIndex++;
+			}
+
+			if (m.length > 1) {
+				return m[1];
+			}
+
+			return false;
+		}
+
+		return false;
+	}
+};
