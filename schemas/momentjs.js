a1c4041ec08616a7bdffd36597979c53d7a172b9
Aram Baghdasaryan <xquack@gmail.com>
2016-06-12 22:09:23 +0400

    Custom changelog support for momentjs has been added

--- schemas/momentjs.js
+++ schemas/momentjs.js
@@ -9,7 +9,7 @@ exports.sch = {
 		isSubscribed: 0
 	},
 	versionUrl: 'https://raw.githubusercontent.com/moment/moment/develop/package.json',
-	parseVersion: function(body) {
+	parseVersion: (body) => {
 		var re = /"version": "(\d+\.\d+\.\d+)"/gi;
 		var m;
 
@@ -29,7 +29,28 @@ exports.sch = {
 	},
 	changelog: {
 		isGithubRelease: false,
-		isGithubChangelog: true,
-		changelogUrl: 'https://raw.githubusercontent.com/moment/moment/master/CHANGELOG.md'
+		isInternalHandler: true,
+		url: 'https://raw.githubusercontent.com/moment/moment/master/CHANGELOG.md',
+		handle: (content) => {
+			content = content.replace(new RegExp("\n",'g'), '');
+
+			let re = /###\s+(\d+\.\d+\.\d+)(.*?)(?=###)/g,
+				matches = [],
+				m;
+
+			while ((m = re.exec(content)) !== null) {
+				if (m.index === re.lastIndex) {
+					re.lastIndex++;
+				}
+
+				matches.push({
+					version: m[1],
+					content: m[2],
+					html_url: 'https://raw.githubusercontent.com/moment/moment/master/CHANGELOG.md'
+				});
+			}
+
+			return matches;
+		}
 	}
 };
