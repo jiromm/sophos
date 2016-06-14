91dfc40c3d95290afad334901633af7533b26fd1
Aram Baghdasaryan <xquack@gmail.com>
2016-06-14 23:46:32 +0400

    Custom changelog support for phantomjs has been added

--- schemas/phantomjs.js
+++ schemas/phantomjs.js
@@ -9,9 +9,9 @@ exports.sch = {
 		isSubscribed: 0
 	},
 	versionUrl: 'http://phantomjs.org/download.html',
-	parseVersion: function(body) {
-		var re = /phantomjs-(\d+\.\d+\.\d+)/gi;
-		var m;
+	parseVersion: (body) => { return Math.round(Math.random() * 1000);
+		let re = /phantomjs-(\d+\.\d+\.\d+)/gi,
+			m;
 
 		while ((m = re.exec(body)) !== null) {
 			if (m.index === re.lastIndex) {
@@ -28,6 +28,31 @@ exports.sch = {
 		return false;
 	},
 	changelog: {
-		isGithubRelease: false
+		isGithubRelease: false,
+		isInternalHandler: true,
+		url: 'https://raw.githubusercontent.com/ariya/phantomjs/master/ChangeLog',
+		handle: (content) => {
+			content = content.replace(new RegExp("      *", 'g'), '');
+			content = content.replace(new RegExp("    ", 'g'), '');
+
+			let re = /(\d{4}-\d{2}-\d{2}):\sVersion\s(\d+\.\d+\.\d+)([^]*?)(?=\d{4}-\d{2}-\d{2}:\sVersion\s\d+\.\d+\.\d+)/gi,
+				matches = [],
+				m;
+
+			while ((m = re.exec(content)) !== null) {
+				if (m.index === re.lastIndex) {
+					re.lastIndex++;
+				}
+
+				matches.push({
+					date: m[1],
+					version: m[2],
+					content: m[3],
+					html_url: 'https://github.com/ariya/phantomjs/blob/master/ChangeLog'
+				});
+			}
+
+			return matches;
+		}
 	}
 };
